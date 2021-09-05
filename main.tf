337b3878c457cbbda5bfd831827c08c403888041
dwilliams0 <90139381+dwilliams0@users.noreply.github.com>
2021-09-05 14:51:48 +0100

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,34 @@
+resource "azurerm_private_dns_zone" "dns_zone" {
+  name                = var.private_dns_zone_name
+  resource_group_name = var.resource_group_name
+  
+  tags = {
+    Environment = var.environment
+    BuildBy     = var.tag_buildby
+    BuildTicket = var.tag_buildticket
+    BuildDate   = var.tag_builddate
+  }
+}  
+
+resource "azurerm_private_dns_zone_virtual_network_link" "dns_link" {
+  name                  = var.private_dns_zone_virtual_network_link
+  resource_group_name   = var.resource_group_name
+  private_dns_zone_name = azurerm_private_dns_zone.dns_zone.name
+  virtual_network_id    = var.virtual_network_id
+}
+
+resource "azurerm_private_dns_a_record" "arecord" {
+  name                = var.private_dns_a_record
+  zone_name           = azurerm_private_dns_zone.dns_zone.name
+  resource_group_name = var.resource_group_name
+  ttl                 = var.ttl
+  records             = var.dns_records
+}
+
+resource "azurerm_private_dns_cname_record" "cname" {
+  name                = var.private_dns_cname_record
+  zone_name           = azurerm_private_dns_zone.dns_zone.name
+  resource_group_name = var.resource_group_name
+  ttl                 = var.ttl
+  record              = var.record
+}
